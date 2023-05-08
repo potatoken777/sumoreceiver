c399a6a63727874d785fe955a6698edd136c7dee
potatoken777 <122539617+potatoken777@users.noreply.github.com>
2023-05-08 08:46:09 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,84 @@
+radio.onReceivedValue(function (name, value) {
+    comment.comment("DO NOT CHANGE!")
+    if (name == "x") {
+        xValue = value
+    }
+    if (name == "y") {
+        yValue = value
+    }
+    if (name == "A") {
+        action = "A"
+    }
+    if (name == "B") {
+        action = "B"
+    }
+    if (name == "AB") {
+        action = "AB"
+    }
+    if (name == "C") {
+        action = "C"
+    }
+    if (name == "D") {
+        action = "D"
+    }
+    if (name == "E") {
+        action = "E"
+    }
+    if (name == "F") {
+        action = "F"
+    }
+})
+let action = ""
+let yValue = 0
+let xValue = 0
+comment.comment("CHANGE THE RADIO GROUP TO YOUR TEAM'S JOYSTICK NUMBER")
+radio.setGroup(25)
+basic.showIcon(IconNames.Yes)
+basic.showString("R")
+basic.forever(function () {
+    if (action == "A") {
+        comment.comment("Insert A-Button Action Between These Comments")
+        comment.comment("Insert A-Button Action Between These Comments")
+        action = "Z"
+    } else if (action == "B") {
+        comment.comment("Insert B-Button Action Between These Comments")
+        comment.comment("Insert B-Button Action Between These Comments")
+        action = "Z"
+    } else if (action == "AB") {
+        comment.comment("Insert A/B-Button Action Between These Comments")
+        comment.comment("Insert A/B-Button Action Between These Comments")
+        action = "Z"
+    } else if (action == "C") {
+        comment.comment("right turn")
+        wuKong.setAllMotor(100, 0)
+        basic.pause(600)
+        wuKong.stopAllMotor()
+        comment.comment("forword 6in")
+        wuKong.setAllMotor(100, 100)
+        basic.pause(500)
+        wuKong.stopAllMotor()
+        comment.comment("Insert C-Button Action Between These Comments")
+        action = "Z"
+    } else if (action == "D") {
+        comment.comment("Insert D-Button Action Between These Comments")
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 60)
+        comment.comment("Insert D-Button Action Between These Comments")
+        action = "Z"
+    } else if (action == "E") {
+        comment.comment("Insert E-Button Action Between These Comments")
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 15)
+        comment.comment("Insert E-Button Action Between These Comments")
+        action = "Z"
+    } else if (action == "F") {
+        comment.comment("left turn")
+        wuKong.setAllMotor(0, 100)
+        basic.pause(600)
+        wuKong.stopAllMotor()
+        comment.comment("Insert F-Button Action Between These Comments")
+        action = "Z"
+    } else {
+        comment.comment("DO NOT CHANGE!")
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, yValue + xValue)
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, yValue - xValue)
+    }
+})
